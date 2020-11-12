8c2736141b8d7944a767184d3ee06cce6861bbf2
TuanNG <tuanng@heligate.com.vn>
2020-11-12 10:07:50 +0700

    Add angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CommonModule } from '@angular/common';
 import { ScreensModule } from './screens/screens.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { ScreensModule } from './screens/screens.module';
     ScreensModule,
     BrowserModule,
     CommonModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
